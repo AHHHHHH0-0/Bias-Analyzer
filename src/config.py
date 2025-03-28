c9c56a3b0035f9783b7b0e58ca2bd6e1c4002f7a
ahliu7 <landrew1018@gmail.com>
2025-03-27 17:48:29 -0700

    added API fetch paramters

--- src/config.py
+++ src/config.py
@@ -7,3 +7,8 @@ load_dotenv()
 # API keys and configurations
 NEWS_API_KEY = os.getenv('NEWS_API_KEY')
 BASE_URL = "https://newsapi.org/v2"
+
+# API fetch parameters
+DAYS_BACK = 7
+LANGUAGE = 'en'
+PAGE_SIZE = 100
