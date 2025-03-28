d020bdcb19028fa8d22a91009a7924e5d9b76579
ahliu7 <landrew1018@gmail.com>
2025-03-27 17:06:28 -0700

    general structure of project

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# src/config.py
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# API keys and configurations
+NEWS_API_KEY = os.getenv('NEWS_API_KEY')
+BASE_URL = "https://newsapi.org/v2"
