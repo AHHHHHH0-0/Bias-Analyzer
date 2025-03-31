58ac35f88c8de71363c2977fdc122bca19f07468
ahliu7 <landrew1018@gmail.com>
2025-03-30 23:56:42 -0700

    build dataset with collect dataset function

---
+++ src/data/build_dataset.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import os
+from src.data.news_api import NewsAPIClient
+from src.data.text_preprocessor import TextPreprocessor
+
+def collect_dataset(topics, sources, days_back=7):
+    """
+    Collect a diverse dataset of news articles
+    
+    Args:
+        topics (list): List of topics to search for
+        sources (list): List of news sources
+        days_back (int): How many days back to search
+        
+    Returns:
+        pandas.DataFrame: Combined dataset
+    """
